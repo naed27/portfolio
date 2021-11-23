e3780e0aaaa2d7e00dd8a3690d4d8a2f71c14f76
Dean <naed221@gmail.com>
2021-11-23 17:20:50 +0800

    fixed the controllers

--- components/CardSearcher/ViewerArea/Components/ControlPanel.tsx
+++ components/CardSearcher/ViewerArea/Components/ControlPanel.tsx
@@ -1,23 +1,38 @@
-import { useCallback } from "react";
+import { useCallback,useMemo } from "react";
 import { YGOCard } from "../../Misc/Types";
 import styles from "../Styles/ControlPanel.module.scss";
 import Controller from './Controller';
+import { getCardLimit } from '../../Misc/Functions';
 
-export default function ControlPanel({card}:{card:YGOCard}){
+interface Props{
+  addToDeck:(deckType: string, card: YGOCard) => void,
+  removeFromDeck:(deckType: string, card: YGOCard) => void,
+  getDeckCardCount:(card: YGOCard, deckType: string) => number,
+  getExistingCardCount:(card: YGOCard) => number,
+  getDeckStatus: (deckType: string) => string
+  card:YGOCard
+}
 
+export default function ControlPanel({props}:{props:Props}){
+
+  const {getExistingCardCount,card} = props
+  const getCardCount = useCallback(getExistingCardCount,[getExistingCardCount]);
+  const getCardMax = useCallback(getCardLimit,[getCardLimit]);
+  const cardCount = useMemo(()=>getCardCount(card),[getCardCount,card]);
+  const cardLimit = useMemo(()=>getCardMax(card),[getCardMax,card]);
 
   return (
     <div className={styles.container}>
       <div className={styles.wrapper}>
         <div className={styles.header}>
-          Stock: {1}
+          Stocks: {`${cardLimit-cardCount}/${cardLimit}`}
         </div>
         <div className={styles.body}>
           <div className={styles.deck}>
-            <Controller card={card} deck={'main'}/>
+            <Controller props={props} deck={'main'}/>
           </div>
           <div className={styles.deck}>
-            <Controller card={card} deck={'side'}/>
+            <Controller props={props} deck={'side'}/>
           </div>
         </div>
       </div>
--- components/CardSearcher/ViewerArea/Components/Controller.tsx
+++ components/CardSearcher/ViewerArea/Components/Controller.tsx
@@ -1,80 +1,51 @@
 import styles from '../Styles/Controller.module.scss';
-import { useRef, useState, useMemo} from 'react';
-import { NotifType,YGOCard } from '../../Misc/Types';
-import { AnimatePresence, motion } from 'framer-motion'
-import Notif from './Notif';
-import DeckStore from '../../Hooks/DeckStore';
-import useOnClickOutside from '../../../../hooks/useOnClickOutside';
-import { controllerMotion } from '../Animations/Controller';
-import { capitalizeFirstLetter,getCardLimit,getCardCategory } from '../../Misc/Functions';
+import { useRef, useState, useMemo, useCallback } from 'react';
+import { YGOCard } from '../../Misc/Types';
+import { capitalizeFirstLetter,getCardCategory } from '../../Misc/Functions';
 
-export default function Controller({card,deck}:{card:YGOCard,deck:string}){
+interface Props{
+  addToDeck:(deckType: string, card: YGOCard) => void,
+  removeFromDeck:(deckType: string, card: YGOCard) => void,
+  getDeckCardCount:(card: YGOCard, deckType: string) => number,
+  getExistingCardCount:(card: YGOCard) => number,
+  getDeckStatus: (deckType: string) => string
+  card:YGOCard
+}
 
-  const {addToDeck,removeFromDeck,getDeckCardCount,getExistingCardCount} = DeckStore();
-  const [notifs,setNotifs] = useState<NotifType[]>([]);
-  const [showcontrols,setShowControls] = useState(false);
-  const [switchLock,setSwitchLock] = useState(false);
+export default function Controller({props,deck}:{props:Props,deck:string}){
+
+  const {addToDeck,removeFromDeck,getDeckCardCount,getDeckStatus,card} = props
   const deckType = deck==='main'?getCardCategory(card):deck;
 
+  const addCard = useCallback(addToDeck,[addToDeck]);
+  const removeCard = useCallback(removeFromDeck,[removeFromDeck]);
+  const fetchDeckStatus = useCallback(getDeckStatus,[getDeckStatus]);
+  const deckStatus = useMemo(()=>fetchDeckStatus(deckType),[fetchDeckStatus,deckType]);
   const cardcount = useMemo(()=>getDeckCardCount(card,deck),[card,deck,getDeckCardCount]);
-  const [overAllCardCount,setOverAllCardCount] = useState(getExistingCardCount(card));
-  const [cardLimit,setCardLimit] = useState(getCardLimit(card));
-
+  
   const buttonRef = useRef(null);
-  useOnClickOutside(buttonRef, () => showcontrols?setShowControls(false):null);
 
-  const notify = (action:string)=>{
-    if(switchLock && action==='remove')return
-    if(action==='add'){
-      setSwitchLock(true);
-      setTimeout(()=>{setSwitchLock(false)},700)
-    }
-    const result = action==='add'?addToDeck(deck,card):removeFromDeck(deck,card);
-    if(result===null)return
-    const {status,message} = result;
-
-    if(status<1)return
-    
-    const id = `notif_${Date.now()}`;
-    const delay = message==='card added'||message==='card removed'?700:3000;
-    const timer = setTimeout(()=>setNotifs((curr)=>curr.filter((n)=>n.id!==id)),delay)
-    const notif = {id,card,timer,message,deck,action}
-    setNotifs((current)=>[...current,notif]);
-  }
+  const control = useCallback((action:string)=>{
+    if(action==='add')return addCard(deckType,card)
+    return removeCard(deckType,card);
+  },[card,deckType,addCard,removeCard])
 
   return (
-    <div className={styles.button} ref={buttonRef}>
-        <div className={styles.notifications}>
-          <AnimatePresence>
-            {notifs.map((notif)=><Notif key={`ntf_${deck}_${notif.id}`} notif={notif} />)}
-          </AnimatePresence>
+    <div className={styles.container} ref={buttonRef}>
+       
+        <div className={styles.label}>
+          {`${capitalizeFirstLetter(deckType)}`}
         </div>
 
-        <div className={styles.label} onClick={()=>{setShowControls((c)=>!c)}}>
-          {`${capitalizeFirstLetter(deckType)} (${cardcount})`}
+        <div className={styles.stats}>
+          {`${deckStatus}`}
         </div>
         
-        <AnimatePresence>
-          {showcontrols&&
-          <motion.div className={styles.controllers}
-          variants={controllerMotion}
-          initial='initial'
-          animate='final'
-          exit='exit'>
-            <div className={styles.controller}onClick={()=>notify('add')}>+
-            {(overAllCardCount===cardLimit)&&
-              <div className={styles.coverWrapper}>
-                <div className={styles.cover}></div>
-              </div>}
-            </div>
-            <div className={styles.controller}onClick={()=>notify('remove')}>-
-            {(cardcount===0)&&
-              <div className={styles.coverWrapper}>
-                <div className={styles.cover}></div>
-              </div>}
-            </div>
-          </motion.div>}
-        </AnimatePresence>
+        <div className={styles.controllers}>
+          <div className={styles.controller}onClick={()=>control('remove')}>-</div>
+          <div className={styles.controller}>{cardcount}</div>
+          <div className={styles.controller}onClick={()=>control('add')}>+</div>
+        </div> 
     </div>
   )
 }
