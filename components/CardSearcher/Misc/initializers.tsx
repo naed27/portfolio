67f0dcb86ca0a86763872cb93cd130e52cd837f2
Dean <naed221@gmail.com>
2022-02-15 21:24:41 +0800

    default val for card game is TCG

--- components/CardSearcher/Misc/initializers.tsx
+++ components/CardSearcher/Misc/initializers.tsx
@@ -12,7 +12,7 @@ export const initialQuery: Query = {
   def:{ min:-1, max:-1 },
   level:{ min:-1, max:-1 },
   limit: -1,
-  cardGame:'',
+  cardGame:'TCG',
 }
 
 export const initializeHolders = ( size: number )=>{
