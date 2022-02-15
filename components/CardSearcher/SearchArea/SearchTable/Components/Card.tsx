72ad7695abe284b8c99fbf21b83515eaeb793017
Dean <naed221@gmail.com>
2022-02-15 23:15:51 +0800

    added a limit indicator to cards if limit is below 3

--- components/CardSearcher/SearchArea/SearchTable/Components/Card.tsx
+++ components/CardSearcher/SearchArea/SearchTable/Components/Card.tsx
@@ -2,12 +2,12 @@ import Image from 'next/image';
 import { YGOCard } from '../../../Misc/globalTypes';
 import styles from '../Styles/Card.module.css';
 import { GlobalContext } from '../../../Misc/globalContext';
-import React, { useCallback, useContext, memo} from 'react';
-import { renderCardLimit } from '../../../Misc/globalFunctions';
+import React, { useCallback, useContext, memo, useMemo} from 'react';
+import { parseLimit, renderCardLimit } from '../../../Misc/globalFunctions';
 
 const Card = ({card}:{card:YGOCard}) => {
 
-  const {setSelectedCard,setSearchIndex,searchedCards} = useContext(GlobalContext);
+  const {setSelectedCard,setSearchIndex,searchedCards,query} = useContext(GlobalContext);
 
   const viewCard = ()=>{
     setSelectedCard(card);
@@ -17,6 +17,8 @@ const Card = ({card}:{card:YGOCard}) => {
   const onDragHandler = useCallback((e:React.DragEvent<HTMLDivElement>)=>{
     e.dataTransfer.setData('card',JSON.stringify(card));
   },[card]);
+
+  const limit = useMemo(()=>parseLimit(query.cardGame,card.banlist_info),[ card, query.cardGame ]);
 
   return (
     <div className={styles.container} 
@@ -31,12 +33,13 @@ const Card = ({card}:{card:YGOCard}) => {
           layout='fill'
           objectFit='contain'
         /> 
+        
+        {limit<3&&(<div className={styles.limitContainer}>
+          {limit}
+        </div>)}
       </div>
 
       <div className={styles.details}>{card.name}</div>
-      <div className={styles.limitContainer}>
-        {renderCardLimit(card)}
-      </div>
     </div>
   );
 
