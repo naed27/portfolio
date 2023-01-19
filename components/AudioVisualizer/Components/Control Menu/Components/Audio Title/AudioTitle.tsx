8826cb60fb160ae85e93f21dafaf65e7c3998551
Dean <naed221@gmail.com>
2023-01-19 17:52:52 +0800

    make the song title fit the box, and animate it if its too long

--- components/AudioVisualizer/Components/Control Menu/Components/Audio Title/AudioTitle.tsx
+++ components/AudioVisualizer/Components/Control Menu/Components/Audio Title/AudioTitle.tsx
@@ -1,14 +1,34 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useRef } from 'react';
 import { GlobalContext } from '../../../../Context/GlobalContext';
 import styles from './AudioTitle.module.scss'
 
 export default function AudioTitle () {
   
   const { audioTitle } = useContext(GlobalContext);
+  const containerRef = useRef<HTMLDivElement>(null);
+  const textRef = useRef<HTMLSpanElement>(null);
+
+  useEffect(()=>{
+    if(!containerRef.current||!textRef.current) return
+    const text = textRef.current
+    const container = containerRef.current
+    if (container.clientWidth < text.clientWidth){
+      text.classList.add(styles.animate)
+      container.style.justifyContent = ''
+    }
+    else {
+      text.classList.remove(styles.animate)
+      container.style.justifyContent = 'center'
+    }
+  },[audioTitle])
   
   return (
-    <div className={styles.container}>
-      <div className={styles.title}>{(audioTitle === null) ? 'No file' : audioTitle}</div> 
+    <div ref={containerRef} className={styles.container}>
+        <span ref={textRef}>
+          {(audioTitle === null) ? 'No file' : audioTitle}
+        </span>  
+        <div className={`${styles.fader} ${styles.faderLeft}`}></div>
+        <div className={`${styles.fader} ${styles.faderRight}`}></div>
     </div>
   )
 }
