f23409ff24c3b6eae54f92bf3aee4258e377f57f
Dean <naed221@gmail.com>
2023-02-07 17:07:28 +0800

    fix input

--- components/CountryInformer/SearchArea/Components/Filter/Components/AdvancedFilter/Components/Population.tsx
+++ components/CountryInformer/SearchArea/Components/Filter/Components/AdvancedFilter/Components/Population.tsx
@@ -1,45 +1,35 @@
 import styles from './Styles/Field.module.scss'
 import { Searcher } from '../../../../../Hooks/useSearcher';
 import { GlobalContext } from '../../../../../../Context/context';
-import { ChangeEvent, useCallback, useContext, useState, useEffect, useMemo } from 'react'
+import { ChangeEvent, useCallback, useContext, useState } from 'react'
 
 export default function Population ({searcher}: {searcher: Searcher}) {
   
-  const search = useCallback(searcher,[searcher]);
-  const [minPlaceHolder,setMinPlaceHolder] = useState('');
-  const [maxPlaceHolder,setMaxPlaceHolder] = useState('');
-
   const {query} = useContext(GlobalContext);
+
+  const search = useCallback(searcher,[searcher]);
+  const [minPlaceHolder,setMinPlaceHolder] = useState(`${query.population.min}`);
+  const [maxPlaceHolder,setMaxPlaceHolder] = useState(`${query.population.max}`);
+
   const [typingTimeout, setTypingTimeout] = useState<NodeJS.Timeout|null>(null);
 
   const minPopulationHandler = (e:ChangeEvent<HTMLInputElement>)=>{
     const input = e.target.value==='' ? -1 : Number(e.target.value)
-    if(isNaN(input)) return
+    if(isNaN(input) || input > 100000000000) return
     
+    setMinPlaceHolder(`${input}`)
     if(typingTimeout)clearTimeout(typingTimeout)
     setTypingTimeout(setTimeout(()=>search({population:{min:input,max:query.population.max}}), 300));
-    setMinPlaceHolder(e.target.value)
   }
 
   const maxPopulationHandler = (e:ChangeEvent<HTMLInputElement>)=>{
     const input = e.target.value==='' ? -1 : Number(e.target.value)
-    if(isNaN(input)) return
+    if(isNaN(input) || input > 100000000000) return
 
+    setMaxPlaceHolder(`${input}`)
     if(typingTimeout)clearTimeout(typingTimeout)
     setTypingTimeout(setTimeout(()=>search({population:{max:input,min:query.population.min}}), 300));
-    setMaxPlaceHolder(e.target.value)
   }
-
-  useEffect(()=>{
-    if(query.population.min === -1 || query.population.max === -1){
-      if(query.population.min === -1 ) setMinPlaceHolder('');
-      if(query.population.max === -1) setMaxPlaceHolder('');
-      return
-    }
-    setMinPlaceHolder(`${query.population.min}`);
-    setMaxPlaceHolder(`${query.population.max}`);
-  },[query.population.min,query.population.max])
-
 
   return (
     <div className={styles.container}>
@@ -52,7 +42,7 @@ export default function Population ({searcher}: {searcher: Searcher}) {
             type="text" 
             onChange={minPopulationHandler} 
             spellCheck="false"
-            value={minPlaceHolder}
+            value={(minPlaceHolder==='-1') ? '' : minPlaceHolder}
           />
         
         <div className={styles.minipad}>
@@ -64,7 +54,7 @@ export default function Population ({searcher}: {searcher: Searcher}) {
             type="text" 
             onChange={maxPopulationHandler} 
             spellCheck="false"
-            value={maxPlaceHolder}
+            value={(maxPlaceHolder==='-1') ? '' : maxPlaceHolder}
           />
         
       </div>
