56a2aac332b47068e004a287970eb222da4490bf
Dean <naed221@gmail.com>
2023-02-07 15:39:21 +0800

    fix lags

--- components/CardSearcher/SearchArea/SearchFields/Components/Atk.tsx
+++ components/CardSearcher/SearchArea/SearchFields/Components/Atk.tsx
@@ -7,80 +7,67 @@ import TextCover from '../../../../../utility/TextCover/TextCover';
 
 function Atk ({searcher}: {searcher:Searcher}){
   
+  const {query} = useContext(GlobalContext);
   const search = useCallback(searcher,[searcher]);
   const [minPlaceHolder,setMinPlaceHolder] = useState('');
   const [maxPlaceHolder,setMaxPlaceHolder] = useState('');
-
   const [typingTimeout, setTypingTimeout] = useState<NodeJS.Timeout|null>(null);
-  const {query} = useContext(GlobalContext);
+
 
   const minAtkHandler = (e:ChangeEvent<HTMLInputElement>)=>{
     const input = e.target.value==='' ? -1 : Number(e.target.value)
-    if(isNaN(input)) return
-    
+    if(isNaN(input) && e.target.value !== '') return
+
+    setMinPlaceHolder(`${input}`)
     if(typingTimeout)clearTimeout(typingTimeout)
     setTypingTimeout(setTimeout(()=>search({atk:{min:input,max:query.atk.max}}), 300));
-    setMinPlaceHolder(e.target.value)
   }
 
   const maxAtkHandler = (e:ChangeEvent<HTMLInputElement>)=>{
     const input = e.target.value==='' ? -1 : Number(e.target.value)
-    if(isNaN(input)) return
+    if(isNaN(input) && e.target.value !== '') return
 
+    setMaxPlaceHolder(`${input}`)
     if(typingTimeout)clearTimeout(typingTimeout)
     setTypingTimeout(setTimeout(()=>search({atk:{max:input,min:query.atk.min}}), 300));
-    setMaxPlaceHolder(e.target.value)
   }
-
-  useEffect(()=>{
-    setMinPlaceHolder('');
-    setMaxPlaceHolder('');
-  },[query.type])
-
-  
-  useEffect(()=>{
-    if(query.atk.min === -1 || query.atk.max === -1){
-      if(query.atk.min === -1 ) setMinPlaceHolder('');
-      if(query.atk.max === -1) setMaxPlaceHolder('');
-      return
-    }
-    setMinPlaceHolder(`${query.atk.min}`);
-    setMaxPlaceHolder(`${query.atk.max}`);
-  },[query.atk.min,query.atk.max])
-
-  
-  const showCoverCondition = useMemo(()=> query.type!=='Monster',[query.type])
 
   return (
     <div className={styles.container}>
 
-      <TextCover className={styles.label} showCover={showCoverCondition}>
+      <TextCover className={styles.label} showCover={query.type!=='Monster'}>
         {`Attack`}
       </TextCover>
 
       <div className={styles.wrapper}>
 
-        <FieldCover className={styles.fieldWrapper} showCover={showCoverCondition}>
+        <FieldCover className={styles.fieldWrapper} showCover={query.type!=='Monster'}>
           <input 
             className={`${styles.input}`} 
             type="text" 
             onChange={minAtkHandler} 
             spellCheck="false"
-            value={minPlaceHolder}
+            value={
+              (query.type!=='Monster') ? '' :
+              (minPlaceHolder==='-1') ? '' : minPlaceHolder
+            }
           />
         </FieldCover>
 
-        <TextCover className={styles.separator} showCover={showCoverCondition}>
+        <TextCover className={styles.separator} showCover={query.type!=='Monster'}>
           {`<`}
         </TextCover>
 
-        <FieldCover className={styles.fieldWrapper} showCover={showCoverCondition}>
+        <FieldCover className={styles.fieldWrapper} showCover={query.type!=='Monster'}>
           <input 
             className={`${styles.input}`} 
             type="text" 
             onChange={maxAtkHandler} 
             spellCheck="false"
-            value={maxPlaceHolder}
+            value={
+              (query.type!=='Monster') ? '' :
+              (maxPlaceHolder==='-1') ? '' : maxPlaceHolder
+            }
           />
         </FieldCover>
 
--- components/CardSearcher/SearchArea/SearchFields/Components/Def.tsx
+++ components/CardSearcher/SearchArea/SearchFields/Components/Def.tsx
@@ -7,77 +7,65 @@ import TextCover from '../../../../../utility/TextCover/TextCover';
 
 export default function Def ({searcher}: {searcher:Searcher}){
   
-  const search = useCallback(searcher,[searcher]);
-  const [minPlaceHolder,setMinPlaceHolder] = useState('');
-  const [maxPlaceHolder,setMaxPlaceHolder] = useState('');
-
   const {query} = useContext(GlobalContext);
+  const search = useCallback(searcher,[searcher]);
+  const [minPlaceHolder,setMinPlaceHolder] = useState(`${query.def.min}`);
+  const [maxPlaceHolder,setMaxPlaceHolder] = useState(`${query.def.max}`);
   const [typingTimeout, setTypingTimeout] = useState<NodeJS.Timeout|null>(null);
 
   const minDefHandler = (e:ChangeEvent<HTMLInputElement>)=>{
     const input = e.target.value==='' ? -1 : Number(e.target.value)
-    if(isNaN(input)) return
+    if(isNaN(input) && e.target.value !== '') return
 
+    setMinPlaceHolder(`${input}`)
     if(typingTimeout)clearTimeout(typingTimeout)
     setTypingTimeout(setTimeout(()=>search({def:{min:input,max:query.def.max}}), 300));
-    setMinPlaceHolder(e.target.value)
   }
 
   const maxDefHandler = (e:ChangeEvent<HTMLInputElement>)=>{
     const input = e.target.value==='' ? -1 : Number(e.target.value)
-    if(isNaN(input)) return
+    if(isNaN(input) && e.target.value !== '') return
 
+    setMaxPlaceHolder(`${input}`)
     if(typingTimeout)clearTimeout(typingTimeout)
     setTypingTimeout(setTimeout(()=>search({def:{max:input,min:query.def.min}}), 300));
-    setMaxPlaceHolder(e.target.value)
   }
-
-  useEffect(()=>{
-    setMinPlaceHolder('');
-    setMaxPlaceHolder('');
-  },[query.type])
-
-  useEffect(()=>{
-    if(query.def.min === -1 || query.def.max === -1){
-      if(query.def.min === -1 ) setMinPlaceHolder('');
-      if(query.def.max === -1) setMaxPlaceHolder('');
-      return
-    }
-    setMinPlaceHolder(`${query.def.min}`);
-    setMaxPlaceHolder(`${query.def.max}`);
-  },[query.def.min,query.def.max])
-
-  const showCoverCondition = useMemo(()=> query.type!=='Monster',[query.type])
 
   return (
     <div className={styles.container}>
 
-      <TextCover className={styles.label} showCover={showCoverCondition}>
+      <TextCover className={styles.label} showCover={query.type!=='Monster'}>
         {`Defense`}
       </TextCover>
 
       <div className={styles.wrapper}>
-        <FieldCover className={styles.fieldWrapper} showCover={showCoverCondition}>
+        <FieldCover className={styles.fieldWrapper} showCover={query.type!=='Monster'}>
           <input 
             className={`${styles.input}`} 
             type="text" 
             onChange={minDefHandler} 
             spellCheck="false"
-            value={minPlaceHolder}
+            value={
+              (query.type!=='Monster') ? '' :
+              (minPlaceHolder==='-1') ? '' : minPlaceHolder
+            }
           />
         </FieldCover>
 
-        <TextCover className={styles.separator} showCover={showCoverCondition}>
+        <TextCover className={styles.separator} showCover={query.type!=='Monster'}>
           {`<`}
         </TextCover>
 
-        <FieldCover className={styles.fieldWrapper} showCover={showCoverCondition}>
+        <FieldCover className={styles.fieldWrapper} showCover={query.type!=='Monster'}>
           <input 
             className={`${styles.input}`} 
             type="text" 
             onChange={maxDefHandler} 
             spellCheck="false"
-            value={maxPlaceHolder}
+            value={
+              (query.type!=='Monster') ? '' :
+              (maxPlaceHolder==='-1') ? '' : maxPlaceHolder
+            }
           />
         </FieldCover>
         
