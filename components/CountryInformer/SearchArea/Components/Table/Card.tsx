82d670d8a4de27fcf437531e67e0b5cfb7402096
Dean <naed221@gmail.com>
2023-02-11 17:17:51 +0800

    first implementation of lazy loading

--- components/CountryInformer/SearchArea/Components/Table/Card.tsx
+++ components/CountryInformer/SearchArea/Components/Table/Card.tsx
@@ -1,5 +1,5 @@
 import styles from './Card.module.scss';
-import React, { useCallback, useContext, memo, useState} from 'react';
+import React, { useCallback, useContext, memo, useState, useRef, useEffect} from 'react';
 import ScrollableDiv from '../../../../../utility/CustomScrollDiv/ScrollableDiv';
 import { Country } from '../../../Types/types';
 import { GlobalContext } from '../../../Context/context';
@@ -14,6 +14,8 @@ const Card = ({ country, invisibleChild = false }:Props) => {
 
   const { setSelectedCountry }  = useContext(GlobalContext);
   const [viewLock, setViewLock] = useState(false);
+  const cardRef = useRef<HTMLDivElement>(null)
+  const [showCard, setShowCard] = useState(false);
 
   const viewCard = useCallback(()=>{
     if(viewLock) return
@@ -21,17 +23,35 @@ const Card = ({ country, invisibleChild = false }:Props) => {
     setSelectedCountry(country);
   },[ country, setSelectedCountry, viewLock])
 
+  const isViewedHandler = useCallback((entries:IntersectionObserverEntry[], observer: IntersectionObserver) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        if (entry.intersectionRatio >= 0.01) {
+          setShowCard(true)
+        }
+      }
+    });
+  },[])
+
+  useEffect(()=>{
+    if(!cardRef.current) return
+
+    const observer = new IntersectionObserver(isViewedHandler);
+  
+    observer.observe(cardRef.current);
+  },[isViewedHandler])
+
   
   if(!country)
     return null
 
   return (
-    <ScrollableDiv className={styles.container} onClick={viewCard} scrollX={{ scrollBorderRadius:`20px`, trackPadding:0.8 }}>
-        <div className={styles.details} >
+    <ScrollableDiv customRef={cardRef} className={styles.container} onClick={viewCard} scrollX={{ scrollBorderRadius:`20px`, trackPadding:0.8 }}>
+        {showCard&&<div className={styles.details} >
           <div className={styles.text}>
             {country.name.common}
           </div>
-        </div>
+        </div>}
     </ScrollableDiv>
   );
 
